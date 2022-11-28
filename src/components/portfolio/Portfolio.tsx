95ec3343dd497df357fa36c6e45667375a399527
DoWhatYouL0ve <william_ross@mailfence.com>
2022-11-28 12:26:40 +0100

    refactoring: replacing counter project with weather forecast project

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -3,7 +3,7 @@ import {StyledBackground} from "../styles/common/Background.styled";
 import { H2 } from "../styles/common/Header2.styled";
 import {PortfolioBox, PortfolioContainer} from "./Portfolio.styled";
 import {PortfolioBoxItem} from "./portfolioBoxItem/PortfolioBoxItem";
-import counter from "./img/counter.jpg";
+import weather from "./img/weather.png";
 import social from "./img/social.jpg";
 import todolist from "./img/todolist.jpg";
 import cards from "./img/cards.jpg";
@@ -12,9 +12,9 @@ export const Portfolio = () => {
 
     const portfolio = [
         {id: '1', title: 'Learning cards', description: 'This is a team work with lots of great features. Feel free to create an account and check its functionality.', img: cards, code: 'https://github.com/DoWhatYouL0ve/Cards', demo: 'https://cards-alpha-jade.vercel.app/'},
-        {id: '2', title: 'To Do List', description: 'This is a test big project with API, Redux, Typescript and Unit testing. To log in please ask me for the crredentials first.', img: todolist, code: 'https://github.com/DoWhatYouL0ve/to_do_list.v2', demo: 'https://dowhatyoul0ve.github.io/to_do_list.v2/'},
-        {id: '3', title: 'Counter', description: 'This is my first small project with different features', img: counter, code: 'https://github.com/DoWhatYouL0ve/counter', demo: 'https://dowhatyoul0ve.github.io/counter/'},
-        {id: '4', title: 'Social Network', description: 'This is a test big project with API, Redux, Typescript and lots of funtionality. To log in please ask me for the crredentials first.', img: social, code: 'https://github.com/DoWhatYouL0ve/social_network', demo: 'https://dowhatyoul0ve.github.io/social_network/'},
+        {id: '2', title: 'To Do List', description: 'This is a test big project with API, Redux, Typescript and Unit testing. To log in please ask me for the credentials first.', img: todolist, code: 'https://github.com/DoWhatYouL0ve/to_do_list.v2', demo: 'https://dowhatyoul0ve.github.io/to_do_list.v2/'},
+        {id: '3', title: 'Weather Forecast', description: 'This is a weather application were you can check the weather for the particular city all over the world.', img: weather, code: 'https://github.com/DoWhatYouL0ve/weather-app', demo: 'https://dowhatyoul0ve.github.io/weather-app/'},
+        {id: '4', title: 'Social Network', description: 'This is a test big project with API, Redux, Typescript and lots of functionality. To log in please ask me for the credentials first.', img: social, code: 'https://github.com/DoWhatYouL0ve/social_network', demo: 'https://dowhatyoul0ve.github.io/social_network/'},
     ]
 
     return <>
